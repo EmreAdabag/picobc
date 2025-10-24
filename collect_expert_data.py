0fe6daaf20d7c07a8ea3c2c4ef1089e8fddee3b6
ea <emreadabag@gmail.com>
2025-10-24 00:36:54 -0400

    still no

--- collect_expert_data.py
+++ collect_expert_data.py
@@ -12,9 +12,9 @@ class ExpertController:
 
     def __init__(self):
         self.kp = 10.0
-        self.kd = 4.0
+        self.kd = 6.0
         self.kp_loaded = 10.0
-        self.kd_loaded = 8.0
+        self.kd_loaded = 12.0
         self.u_clip = 4.0
 
     def act(self, env: PickAndPlaceEnv) -> torch.Tensor:
@@ -30,7 +30,7 @@ class ExpertController:
 
 
 def collect_expert_dataset(N: int, path: str, multi_task: bool):
-    B = 20
+    B = 1
     assert int(N) % B == 0
     env = PickAndPlaceEnv(batch_size=B, multi_task=multi_task)
     ctrl = ExpertController()
@@ -39,15 +39,19 @@ def collect_expert_dataset(N: int, path: str, multi_task: bool):
     H, W = int(env.H), int(env.W)
     frames_all = torch.empty(max_steps, B, H, W, 3, dtype=torch.uint8)
     poss_all = torch.empty(max_steps, B, 2, dtype=torch.float32)
+    vel_all = torch.empty(max_steps, B, 2, dtype=torch.float32)
     u_all = torch.empty(max_steps, B, 2, dtype=torch.float32)
     obj_pos_all = torch.empty(max_steps, B, 2, dtype=torch.float32)
+    expert_ctrl_pos_all = torch.empty(max_steps, B, 2, dtype=torch.float32)
 
     root = zarr.open_group(path, mode='w')
     img_arr = root.create_array('data/img', shape=(0, H, W, 3), dtype=np.uint8, chunks=(1000, H, W, 3))
     state_arr = root.create_array('data/state', shape=(0, 2), dtype=np.float32, chunks=(10000, 2))
+    vel_arr = root.create_array('data/vel', shape=(0, 2), dtype=np.float32, chunks=(10000, 2))
     cmd_arr = root.create_array('data/cmd', shape=(0, 2), dtype=np.float32, chunks=(10000, 2))
     action_arr = root.create_array('data/action', shape=(0, 2), dtype=np.float32, chunks=(10000, 2))
     obj_pos_arr = root.create_array('meta/obj_pos', shape=(0, 2), dtype=np.float32, chunks=(10000, 2))
+    expert_ctrl_pos_arr = root.create_array('meta/expert_ctrl_pos', shape=(0, 2), dtype=np.float32, chunks=(10000, 2))
     
     episode_ends = []
     goal_positions = []
@@ -65,19 +69,21 @@ def collect_expert_dataset(N: int, path: str, multi_task: bool):
         active = torch.ones(B, dtype=torch.bool)
         done_step = torch.full((B,), -1, dtype=torch.long)
         t = 0
-        frames_all[t] = env.current_frame().to("cpu")
-        poss_all[t] = env.agent_pos.to("cpu")
-        u_all[t] = torch.zeros(B, 2)
-        for b in range(B):
-            obj_idx = int(env._target_object_idx[b].item())
-            obj_pos_all[t, b] = env.objects_pos[b, obj_idx].to(torch.float32).to("cpu")
-        t += 1
+        # frames_all[t] = env.current_frame().to("cpu")
+        # poss_all[t] = env.agent_pos.to("cpu")
+        # u_all[t] = torch.zeros(B, 2)
+        # for b in range(B):
+        #     obj_idx = int(env._target_object_idx[b].item())
+        #     obj_pos_all[t, b] = env.objects_pos[b, obj_idx].to(torch.float32).to("cpu")
+        # t += 1
         
         while bool(active.any()) and t < max_steps:
+            expert_ctrl_pos_all[t] = torch.where((~env.picked).unsqueeze(-1), env.object_pos, env.goal_center)
             u = ctrl.act(env)
             out = env.step(u)
             frames_all[t] = env.current_frame().to("cpu")
             poss_all[t] = env.agent_pos.to("cpu")
+            vel_all[t] = env.agent_vel.to("cpu")
             u_all[t] = u.to("cpu")
             for b in range(B):
                 obj_idx = int(env._target_object_idx[b].item())
@@ -89,20 +95,27 @@ def collect_expert_dataset(N: int, path: str, multi_task: bool):
             t += 1
 
         for b in range(B):
-            t_end = int(done_step[b].item()) + 1 if int(done_step[b].item()) >= 0 else t
+            t_end = int(done_step[b].item())
+            if t_end < 0:
+                print("INCOMPLETE DEMONSTRATION")
+                t_end = max_steps
             cmd = env.command[b].to(torch.float32).cpu().numpy().copy()
             
             img_arr.resize((total_samples + t_end, H, W, 3))
             state_arr.resize((total_samples + t_end, 2))
+            vel_arr.resize((total_samples + t_end, 2))
             cmd_arr.resize((total_samples + t_end, 2))
             action_arr.resize((total_samples + t_end, 2))
             obj_pos_arr.resize((total_samples + t_end, 2))
+            expert_ctrl_pos_arr.resize((total_samples + t_end, 2))
             
             img_arr[total_samples:total_samples+t_end] = frames_all[:t_end, b].numpy()
             state_arr[total_samples:total_samples+t_end] = poss_all[:t_end, b].numpy()
+            vel_arr[total_samples:total_samples+t_end] = vel_all[:t_end, b].numpy()
             cmd_arr[total_samples:total_samples+t_end] = np.tile(cmd, (t_end, 1))
             action_arr[total_samples:total_samples+t_end] = u_all[:t_end, b].numpy()
             obj_pos_arr[total_samples:total_samples+t_end] = obj_pos_all[:t_end, b].numpy()
+            expert_ctrl_pos_arr[total_samples:total_samples+t_end] = expert_ctrl_pos_all[:t_end, b].numpy()
             total_samples += t_end
             episode_ends.append(total_samples)
     
@@ -125,6 +138,9 @@ def generate_expert_demo_video(batch_size: int = 4, episodes: int = 1, multi_tas
             steps += 1
             if bool(out["success"].all()):
                 break
+            if steps > 1000:
+                print("TIMEOUT")
+                break
 
         # Save one concatenated video for this episode across the batch
         path = f"episode_{ep}.mp4"
@@ -135,7 +151,7 @@ def generate_expert_demo_video(batch_size: int = 4, episodes: int = 1, multi_tas
 
 if __name__ == "__main__":
     multi_task=False
-    demos = 1000
+    demos = 100
     # generate_expert_demo_video(multi_task=multi_task)
     if 1:
-        collect_expert_dataset(demos, f"datasets/expert_{demos}_singletask.zarr", multi_task=multi_task)
+        collect_expert_dataset  (demos, f"datasets/expert_{demos}_singletask_slow.zarr", multi_task=multi_task)
